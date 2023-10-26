17c75ca421c1c318330ca90e30e342453d16e67f
Parth <parth@codamusic.net>
2023-10-26 14:28:52 +0530

    Revert "Revert "- add url call back""
    This reverts commit fd8e9f45e7d3465b99b4768d58cbe887f15db549.

--- kotlin-audio/src/main/java/com/doublesymmetry/kotlinaudio/players/BaseAudioPlayer.kt
+++ kotlin-audio/src/main/java/com/doublesymmetry/kotlinaudio/players/BaseAudioPlayer.kt
@@ -312,8 +312,9 @@ abstract class BaseAudioPlayer internal constructor(
      * @param item The [AudioItem] to replace the current one.
      */
     open fun load(item: AudioItem) {
-        val mediaSource = getMediaSourceFromAudioItem(item)
-        exoPlayer.addMediaSource(mediaSource)
+        getMediaSourceFromAudioItem(item) {
+            exoPlayer.addMediaSource(it)
+        }
         exoPlayer.prepare()
     }
 
@@ -397,46 +398,55 @@ abstract class BaseAudioPlayer internal constructor(
             .build()
     }
 
-    protected fun getMediaSourceFromAudioItem(audioItem: AudioItem): MediaSource {
-        val factory: DataSource.Factory
-        val uri = Uri.parse(audioItem.audioUrl)
-        val mediaItem = getMediaItemFromAudioItem(audioItem)
+    protected fun getMediaSourceFromAudioItem(
+        audioItem: AudioItem,
+        handler: (MediaSource) -> Unit
+    ) {
+        audioItem.getSourceUrl { it ->
+            val uri = Uri.parse(it)
+            val mediaItem = MediaItem.Builder()
+                .setUri(it)
+                .setTag(AudioItemHolder(audioItem))
+                .build()
 
-        val userAgent =
-            if (audioItem.options == null || audioItem.options!!.userAgent.isNullOrBlank()) {
-                Util.getUserAgent(context, APPLICATION_NAME)
-            } else {
-                audioItem.options!!.userAgent
-            }
-
-        factory = when {
-            audioItem.options?.resourceId != null -> {
-                val raw = RawResourceDataSource(context)
-                raw.open(DataSpec(uri))
-                DataSource.Factory { raw }
-            }
-            isUriLocal(uri) -> {
-                DefaultDataSourceFactory(context, userAgent)
-            }
-            else -> {
-                val tempFactory = DefaultHttpDataSource.Factory().apply {
-                    setUserAgent(userAgent)
-                    setAllowCrossProtocolRedirects(true)
-
-                    audioItem.options?.headers?.let {
-                        setDefaultRequestProperties(it.toMap())
-                    }
+            val userAgent =
+                if (audioItem.options == null || audioItem.options!!.userAgent.isNullOrBlank()) {
+                    Util.getUserAgent(context, APPLICATION_NAME)
+                } else {
+                    audioItem.options!!.userAgent
                 }
 
-                enableCaching(tempFactory)
-            }
-        }
+            val factory: DataSource.Factory = when {
+                audioItem.options?.resourceId != null -> {
+                    val raw = RawResourceDataSource(context)
+                    raw.open(DataSpec(uri))
+                    DataSource.Factory { raw }
+                }
+                isUriLocal(uri) -> {
+                    DefaultDataSourceFactory(context, userAgent)
+                }
+                else -> {
+                    val tempFactory = DefaultHttpDataSource.Factory().apply {
+                        setUserAgent(userAgent)
+                        setAllowCrossProtocolRedirects(true)
 
-        return when (audioItem.type) {
-            MediaType.DASH -> createDashSource(mediaItem, factory)
-            MediaType.HLS -> createHlsSource(mediaItem, factory)
-            MediaType.SMOOTH_STREAMING -> createSsSource(mediaItem, factory)
-            else -> createProgressiveSource(mediaItem, factory)
+                        audioItem.options?.headers?.let {
+                            setDefaultRequestProperties(it.toMap())
+                        }
+                    }
+
+                    enableCaching(tempFactory)
+                }
+            }
+
+            handler(
+                when (audioItem.type) {
+                    MediaType.DASH -> createDashSource(mediaItem, factory)
+                    MediaType.HLS -> createHlsSource(mediaItem, factory)
+                    MediaType.SMOOTH_STREAMING -> createSsSource(mediaItem, factory)
+                    else -> createProgressiveSource(mediaItem, factory)
+                }
+            )
         }
     }
 
